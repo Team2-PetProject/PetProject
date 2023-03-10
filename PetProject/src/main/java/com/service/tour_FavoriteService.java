7feb41d016fe833f954c633712ec6ff96fdd8319
Max <Max@192.168.219.103>
2023-03-10 18:56:57 +0900

    (에러1)

--- PetProject/src/main/java/com/service/tour_FavoriteService.java
+++ PetProject/src/main/java/com/service/tour_FavoriteService.java
@@ -1,14 +1,12 @@
 package com.service;
 
-import java.util.List;
-
 import com.dao.tour_FavoriteDAO;
 
 public class tour_FavoriteService {
 	tour_FavoriteDAO dao;
 
 	public tour_FavoriteService() {
-		super();
+		super()
 		dao = new tour_FavoriteDAO();
 	}
 
