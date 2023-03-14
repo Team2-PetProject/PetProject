b7374835250f4d1e910bcdc3b5afdbbb8d31fda9
Max <Max@192.168.219.103>
2023-03-14 15:49:59 +0900

    (찜servlet작업 끝)

--- PetProject/src/main/java/com/controller/favorite/FavoriteUIServlet.java
+++ PetProject/src/main/java/com/controller/favorite/FavoriteUIServlet.java
@@ -2,15 +2,16 @@ package com.controller.favorite;
 
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.dto.item_FavoriteDTO;
 import com.dto.tour_FavoriteDTO;
@@ -24,40 +25,44 @@ public class FavoriteUIServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-//		HttpSession session=request.getSession();
+		HttpSession session=request.getSession();
 //		MemberDTO memberdto=(MemberDTO)session.getAttribute("login");
 //		String member_code=memberdto.getMember_code();
-//		String nextPage="main";
+		String nextPage="LoginUIServlet";
 
-//		if (memberdto==null) {
-//		item_FavoriteService itemService = new item_FavoriteService();
-//		tour_FavoriteService tourService = new tour_FavoriteService();
+//		if (memberdto!=null) {
+//		List<tour_FavoriteDTO> tourList = tour_Service.getList(member_code);
+//		List<item_FavoriteDTO> itemList = item_Service.getList(member_code);
+		
 		item_Service = new item_FavoriteService();
 		tour_Service = new tour_FavoriteService();
-//		List<item_FavoriteDTO> itemList = itemService.getList("111");
-//		List<tour_FavoriteDTO> tourList = tourService.getList("111");
-		List<item_FavoriteDTO> itemList = item_Service.getList("111");
 		List<tour_FavoriteDTO> tourList = tour_Service.getList("111");
-		HashMap<Integer, Object> map= new HashMap<Integer, Object>();
-		System.out.println(itemList.size());
-			for (int i = 0; i < itemList.size(); i++) {
-				map.put(i,itemList);
-				for (int j = itemList.size()+1; j < itemList.size()+tourList.size(); j++) {
-					map.put(j, tourList);
-				}
-			}
-			Set set=map.keySet();
-			Iterator<Integer> keys=map.keySet().iterator();
-			while (keys.hasNext()){
-				int key=keys.next();
-				System.out.println(key);
-			}
-//			
+		List<item_FavoriteDTO> itemList = item_Service.getList("111");
+		HashMap<Integer, item_FavoriteDTO> itemMap= new HashMap<Integer,item_FavoriteDTO>();
+		HashMap<Integer, tour_FavoriteDTO> tourMap= new HashMap<Integer, tour_FavoriteDTO>();	
+		for (int i = 0; i < itemList.size(); i++) {
+			itemMap.put(i, itemList.get(i));
+		}
+		for (int i = 0; i < tourList.size(); i++) {
+			tourMap.put(i, tourList.get(i));
+		}
+		Set<Integer> keySet=itemMap.keySet();
+		for (Integer key : keySet) {
+			System.out.println(key+":"+ itemMap.get(key));
+		}
+		Set<Integer> keySet2=tourMap.keySet();
+		for (Integer key : keySet2) {
+			System.out.println(key+":"+ tourMap.get(key));
+		}
+		request.setAttribute("item_Favorite", itemMap);	
+		request.setAttribute("tour_Favorite", tourMap);
+		RequestDispatcher dis=request.getRequestDispatcher(nextPage);
+		dis.forward(request, response);
+		
 //		}else {
 //			session.setAttribute("mesg", "로그인을 부탁드립니다.");
-//			nextPage="LoginUIServlet";
+//			response.sendRedirect(nextPage);
 //		}
-//		response.sendRedirect(nextPage);
 
 	}
 
