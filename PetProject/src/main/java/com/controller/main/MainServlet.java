8f1ac5bb0845ffd4039079dd600b03c652300496
Dami Park <ekal0323@gmail.com>
2023-03-18 15:21:19 +0900

    java 패키지 수정

--- PetProject/src/main/java/com/controller/main/MainServlet.java
+++ PetProject/src/main/java/com/controller/main/MainServlet.java
@@ -1,25 +1,36 @@
 package com.controller.main;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/")
+import com.dto.GoodsDTO;
+import com.service.GoodsService;
+
+/**
+ * Servlet implementation class MainServlet
+ */
+@WebServlet("/main")
 public class MainServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    public MainServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.sendRedirect("main.jsp");
+				
+		GoodsService service = new GoodsService();
+		List<GoodsDTO> list = service.goodsList("");
+			request.setAttribute("goodsList", list);
+		RequestDispatcher dis = request.getRequestDispatcher("main.jsp");
+		dis.forward(request, response);
 	}
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
