704c96af837518909ee7ee379aa583832cc200d3
Hyerim <Hyerim@DESKTOP-SA9UG1L>
2023-03-28 14:45:29 +0900

    상품상세 retrieve servlet 수정

--- PetProject/src/main/java/com/controller/item/ItemRetrieveServlet.java
+++ PetProject/src/main/java/com/controller/item/ItemRetrieveServlet.java
@@ -20,7 +20,7 @@ public class ItemRetrieveServlet extends HttpServlet {
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		String item_code = request.getParameter("item_code");
+		String item_code = request.getParameter("itemCode");
 		//System.out.println("상품 코드 : " + item_code);
 		
 //		String item_code = "toy06";
